4c2c7ec0b34f982c4209a923a3967af4cf89ff98
Rodrigo <rgarciar91@gmail.com>
2022-05-01 20:52:40 -0500

    Desafio 2 y 3 - NavBar y landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar.js'
+import ItemListContainer from './components/ItemListContainer';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <ItemListContainer greetings='Bienvenidos a Checkpoint'></ItemListContainer>
+      <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
     </div>
   );
 }
